be58a34401140bdbdabd7f61efbd4cf1ecc59e97
Devipriya622 <132970736+Devipriya622@users.noreply.github.com>
2026-04-10 13:09:22 +0530

    Rotated Sorted Array
    Implement a search function for a rotated sorted array.

---
+++ Practice_Problems/MQ31.c
@@ -0,0 +1,9 @@
+//Search in Rotated Sorted Array
+bool search(int* nums, int numsSize, int target) {
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]==target){
+            return true;
+        }
+    }
+    return false;
+}

46fa35baf2d58a00d839e1f5d43ae93f3ecbf88f
Devipriya622 <132970736+Devipriya622@users.noreply.github.com>
2026-04-22 12:35:11 +0530

    File operations: cp, mv, rm, touch
    Added Linux command examples for file operations.

---
+++ Practice_Problems/linux1.sh
@@ -0,0 +1,8 @@
+How do you copy a file from /home/user to /tmp? 
+cp /home/user/filename /tmp/
+
+Write a command to rename a file data.txt to info.txt.
+mv data.txt info.txt
+
+How do you delete a directory and all its contents?
+rm -r directory_name

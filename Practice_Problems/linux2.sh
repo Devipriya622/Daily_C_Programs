ff811273e53f88cb924fd1996e54bdd9fd1d56b1
Devipriya622 <132970736+Devipriya622@users.noreply.github.com>
2026-04-22 12:37:05 +0530

    Viewing files: cat, less, head, tail

---
+++ Practice_Problems/linux2.sh
@@ -0,0 +1,8 @@
+Show how to display the first 10 lines of a file. 
+head filename
+
+What command would you use to view a file page by page? 
+less filename
+
+How do you continuously monitor the last lines of a log file? 
+tail -f filename

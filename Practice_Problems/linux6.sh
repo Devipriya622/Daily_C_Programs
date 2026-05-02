dd64af8a302c7814e335b1aa0c72c4f16cd47285
Devipriya622 <132970736+Devipriya622@users.noreply.github.com>
2026-05-02 14:57:17 +0530

    Loops

---
+++ Practice_Problems/linux6.sh
@@ -0,0 +1,15 @@
+Write a for loop to print numbers from 1 to 10.
+
+for i in {1..10}
+do
+    echo $i
+done
+
+
+
+Create a while loop that reads lines from a file until EOF. 
+
+while IFS= read -r line
+do
+  echo "$line"
+done <data.txt
